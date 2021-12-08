d475f5b5c7c856fdd2925360ba819a55a4fc81e9
Dorel Pislan <dorel.pislan@caphyon.com>
2021-12-08 15:20:47 +0200

    Some work for part 2

--- Day8.js
+++ Day8.js
@@ -3,7 +3,7 @@ const util = require('./util.js');
 const DorelUtil = require('./DorelUtil.js');
 
 
-let inputs = util.MapInput('./Input8.txt', (aElem) => {
+let inputs = util.MapInput('./Input8Sample.txt', (aElem) => {
 
     let pair = aElem.split(' | ');
   
@@ -14,17 +14,16 @@ let inputs = util.MapInput('./Input8.txt', (aElem) => {
   },
     '\r\n');
 
-//                  0       1       2        3        4        5       6         7       8          9
-const digits = [ 'abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg' ];
-const segCount=[    6,      2,      5,       5,       4,       5,      6 ,       3,      7,         6     ];
+//                         0       1       2        3        4        5       6         7       8          9
+const kDigsSegs     = [ 'abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg' ];
+const kDigsSegsCount= [    6,      2,      5,       5,       4,       5,      6 ,       3,      7,         6    ];
 
 let SegCountToDigit = new Map();
 SegCountToDigit.set(2, [1] );
-SegCountToDigit.set(2, [1] ),
 SegCountToDigit.set(3, [7] ); 
 SegCountToDigit.set(4, [4] ),
 SegCountToDigit.set(5, [2, 3, 5] );
-SegCountToDigit.set(6, [6] );
+SegCountToDigit.set(6, [0, 6, 9] );
 SegCountToDigit.set(7, [8] );
  
 
@@ -71,32 +70,70 @@ function part2() {
 
 function DetectMapForInput(aWiresArray )
 {
-    let map  = new Map();
+    let map  = new Map([
+        ['a', 'd'], 
+        ['b', 'e'],
+        ['c', 'a'],
+        ['d', 'f'],
+        ['e', 'g'],
+        ['f', 'b'],
+        ['g', 'c']
+    ]);
 
-    for (let wi = 0; aWiresArray.length; wi++)
-    {
-        let crtStr = aWiresArray[wi];
-        let crtStrLength = crtStr.length;
+    let inputFor1 = GetElemByLength(aWiresArray, kDigsSegsCount[1] );
+    let realSegsFor1 = kDigsSegs[1];
 
-        if  ( (crtStrLength == 2) || 
-              (crtStrLength == 4) ||
-              (crtStrLength == 3) || 
-              (crtStrLength == 7) )
-        {
+    let inputFor7 = GetElemByLength(aWiresArray, kDigsSegsCount[7] );
+    let realSegsFor7 = kDigsSegs[7];
 
-          //let goodDigitIndex = (crtStrLength == 2) ? ;
-          for (let ci = 0; ci < crtStrLength; ci ++ )
-          {
-            let ch = crtStr.charAt(ci);
-            let possib = map[ci];
+    let inputFor4 = GetElemByLength(aWiresArray, kDigsSegsCount[4] );
+    let realSegsFor4 = kDigsSegs[4];
 
-            if (map[ch] == null)
-              map[ch] = new Array();
+    let inputFor8 = GetElemByLength(aWiresArray, kDigsSegsCount[8] );
+    let realSegsFor8 = kDigsSegs[8];
 
-            map[ch].push(ch);
-          }
-        }
-    }
-    
+    let decoded1 = DecodeDigit(inputFor1, map);
+    let decoded4 = DecodeDigit(inputFor4, map);
+    let decoded7 = DecodeDigit(inputFor4, map);
+    let decoded8 = DecodeDigit(inputFor8, map);
+      
    return map;
+}
+
+function GetElemByLength(aArray, aReqLength)
+{
+    for(let i = 0; i < aArray.length; i++)
+    {
+        if(aArray[i].length == aReqLength)
+          return aArray[i];
+
+    }
+    return null;
+}
+
+function MapIsValid(aMap, aWiresArray)
+{
+    for(let wi = 0 ; wi < aWiresArray.length; wi++)
+    {        
+        let decodedInput = DecodeDigit(aWiresArray[wi], aMap);
+    }
+}
+
+function DecodeDigit(aInputDigit, aMap)
+{
+   let decodedChars = new Array(9);
+
+   let inputChars = aInputDigit.split('');
+
+   for(let ici = 0; ici < inputChars.length; ici++ )        
+   {
+       let outChar = aMap[ inputChars[ici] ];
+
+       if (outChar == null)
+         outChar = 'X';
+
+       decodedChars[ici] = outChar;         
+   }
+   return inputChars
+
 }
