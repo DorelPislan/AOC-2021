1c1776ab029af62f41ba8ef316fdc7b926b0cf95
Dorel Pislan <dorel.pislan@caphyon.com>
2021-12-11 20:26:46 +0200

    Added solution for day 9 part 2

--- Day9.js
+++ Day9.js
@@ -4,18 +4,62 @@ const DorelUtil = require('./DorelUtil.js');
 
 
 let inputLines = fs.readFileSync('input9.txt','utf8').split(/\r?\n/);
-const NO_OF_ROWS = inputLines.length;
+
+const NO_OF_ROWS    = inputLines.length;
 const NO_OF_COLUMNS = inputLines[0].length;
 
-/*
-function StringToArray(crtVal, index, arr)
-{
-    let arr = crtVal.split();
-    return arr;
+class Basin {
+    constructor(i, j) {
+        this.x = i;
+        this.y = j;
+
+        this.points = new Array();
+        this.points.push( GetCellIndex(i, j))
+    }
+
+    GetSize()
+    {
+        return this.points.length;
+    };
+
+    
+    AnalyzePoint(i, j, cell, dirStr, x, y)
+    {
+        let pointVal   = GetCell(inputLines, i, j, -1);
+
+        //console.log('Analyzing ' + dirStr + '=(' + i + ',' + j + ')=' + pointVal + ' of point (' + x + ',' + y + ') Val= ' +  GetCell(inputLines, x, y, -1) );
+        
+        if ( (pointVal >= 0) &&  (pointVal != 9) && (pointVal >= cell))
+        { 
+            let pointIndex = GetCellIndex(i, j);
+            if (this.points.indexOf(pointIndex) == -1 )
+            {
+              this.points.push( pointIndex );
+              //console.log("Added point " + i + "," + j + " Val=" + pointVal + " FlatIndex=" + pointIndex);
+              this.ExpandFromPoint(i, j);
+            }
+        }
+    };
+
+    ExpandFromPoint( i, j )
+    {
+        let cell  = GetCell(inputLines, i, j, -1);
+
+        this.AnalyzePoint(i - 1 , j,     cell, '   UP', i, j); //UP        
+        this.AnalyzePoint(i     , j + 1, cell, 'RIGHT', i, j); //RIGHT    
+        this.AnalyzePoint(i + 1 , j,     cell, ' DOWN', i, j); //DOWN        
+        this.AnalyzePoint(i     , j - 1, cell, ' LEFT', i, j); //LEFT        
+    }
+
+    Expand()
+    {
+        this.ExpandFromPoint(this.x, this.y);
+    };
 }
 
-//let input2 = inputLines.map( StringToArray );
-*/
+let part1Basins = new Array();
+let part2Basins = new Array();
+
 part1();
 
 part2();
@@ -28,12 +72,12 @@ function part1() {
    {
        for (let j = 0; j < NO_OF_COLUMNS; j++)
        {
-           let up    = GetCell(inputLines, i - 1, j);
-           let down  = GetCell(inputLines, i + 1, j);
-           let left  = GetCell(inputLines, i,     j - 1);
-           let right = GetCell(inputLines, i,     j + 1);
+           let up    = GetCell(inputLines, i - 1, j,     10);
+           let down  = GetCell(inputLines, i + 1, j,     10);
+           let left  = GetCell(inputLines, i,     j - 1, 10);
+           let right = GetCell(inputLines, i,     j + 1, 10);
 
-           let cell = GetCell(inputLines, i, j);
+           let cell = GetCell(inputLines, i, j, 10);
            if ( (cell < up) && 
                 (cell < down) && 
                 (cell < left) && 
@@ -41,6 +85,8 @@ function part1() {
            {
                //this cell is a low point
                totalRisk += cell + 1;
+
+               part1Basins.push( new Basin(i, j));
            }
        }       
    }
@@ -50,12 +96,39 @@ function part1() {
 }
 
 function part2() {
-    
    
-    console.log("2: Score from Middle is: "  );
+    for (let i = 0; i < NO_OF_ROWS; i++)
+    {
+        for (let j = 0; j < NO_OF_COLUMNS; j++)
+        {
+            let up    = GetCell(inputLines, i - 1, j,     10);
+            let down  = GetCell(inputLines, i + 1, j,     10);
+            let left  = GetCell(inputLines, i,     j - 1, 10);
+            let right = GetCell(inputLines, i,     j + 1, 10);
+ 
+            let cell  = GetCell(inputLines, i, j, 10);
+
+            if ( (cell < up) && 
+                 (cell < down) && 
+                 (cell < left) && 
+                 (cell < right) )
+            {
+                //this cell is a low point
+                let basin = new Basin(i, j);
+                basin.Expand();
+                part2Basins.push(basin);
+            }
+        }       
+    }
+    part2Basins.sort( function(a, b) {
+        return b.GetSize() - a.GetSize();
+      } );
+
+    let totalSize  = part2Basins[0].GetSize() * part2Basins[1].GetSize() *  part2Basins[2].GetSize() ;
+    console.log("2: Total size of 3 largest basins is: " + totalSize);
 }
 
-function GetCell(aInputLInes, i, j)
+function GetCell(aInputLInes, i, j, aInvalidValue)
 {
     if ( (i >= 0)  && 
          (i < NO_OF_ROWS) && 
@@ -65,5 +138,10 @@ function GetCell(aInputLInes, i, j)
         let index = aInputLInes[i][j];
         return parseInt(aInputLInes[i][j]);
     }
-    return 10;
+    return aInvalidValue;
+}
+
+function GetCellIndex(i, j)
+{
+    return i * NO_OF_COLUMNS + j;
 }
